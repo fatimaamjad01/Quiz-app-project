433043139948ad46c33a42a5fae8f3d2c7108552
Fatima Amjad <amjadfatima252@gmail.com>
2025-07-14 12:50:01 +0500

    fixed desgin issue & missing import & renamed folder

--- src/pages/QuizIntroScreen.jsx
+++ src/pages/QuizIntroScreen.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const QuizIntroScreen = () => {
   const [quote, setQuote] = useState("");
-  
+
   useEffect(() => {
     fetch("https://dummyjson.com/quotes/random")
       .then((response) => response.json())
@@ -13,19 +13,20 @@ const QuizIntroScreen = () => {
       })
       .catch((error) => console.error("Error fetching quote:", error));
   }, []);
-  
+
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
       style={{
-        backgroundImage: "url('https://t4.ftcdn.net/jpg/03/72/52/73/240_F_372527316_yFACBKtjrQkejBIonuxUGiCGlBOq9i7y.webp')",
+        backgroundImage:
+          "url('https://t4.ftcdn.net/jpg/03/72/52/73/240_F_372527316_yFACBKtjrQkejBIonuxUGiCGlBOq9i7y.webp')",
       }}
     >
       <div className="text-center px-6 sm:px-8 md:px-16 lg:px-24">
         <p className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-black mb-6">
           Welcome to the Quiz App
         </p>
-        
+
         <div className="mt-8 p-6 bg-white bg-opacity-80 rounded-lg shadow-xl max-w-xl mx-auto">
           <p className="text-base sm:text-lg md:text-xl font-semibold text-black leading-relaxed">
             {quote.quote ? `"${quote.quote}"` : "Loading quote..."} <br />
@@ -42,6 +43,6 @@ const QuizIntroScreen = () => {
       </div>
     </div>
   );
-}
+};
 
 export default QuizIntroScreen;
--- src/pages/QuizQuestions.jsx
+++ src/pages/QuizQuestions.jsx
@@ -10,23 +10,22 @@ const QuizQuestions = () => {
   const [score, setScore] = useState(0);
   const [timer, setTimer] = useState(30);
   useEffect(() => {
-    if(timer>0 && !selectedAnswer){
+    if (timer > 0 && !selectedAnswer) {
       const countdown = setTimeout(() => {
         setTimer(timer - 1);
       }, 1000);
       return () => clearTimeout(countdown);
-    }else if (timer === 0) {
+    } else if (timer === 0) {
       handleNext();
     }
-
-
   }, [timer, selectedAnswer]);
 
   useEffect(() => {
     setTimer(30);
   }, [currentQuestion]);
 
-  if (!questions) return <div className="text-center text-white">No quiz data!</div>;
+  if (!questions)
+    return <div className="text-center text-white">No quiz data!</div>;
 
   const currentQ = questions[currentQuestion];
   const isLastQuestion = currentQuestion === questions.length - 1;
@@ -39,7 +38,9 @@ const QuizQuestions = () => {
   const handleNext = () => {
     if (isLastQuestion) {
       alert(`Quiz Complete! Score: ${score}/${questions.length} 
-        feedback: ${score >= questions.length / 2 ? "Good Job!" : "Better luck next time!"}`);
+        feedback: ${
+          score >= questions.length / 2 ? "Good Job!" : "Better luck next time!"
+        }`);
       return;
     }
     setCurrentQuestion(currentQuestion + 1);
@@ -49,14 +50,16 @@ const QuizQuestions = () => {
   return (
     <div className=" bg-black text-white p-4">
       <div className="text-center mb-6">
-        <h1 className="text-2xl">{category} Quiz</h1>
+        <h1 className="text-xl">{category} Quiz</h1>
         <div className="mt-4 text-center text-gray-400 rounded-2xl p-4">
           <p>Level: {currentQ.difficulty_level}</p>
         </div>
         <div className="flex justify-center space-x-4 mt-2">
           <span>Score: {score}</span>
-          <span className={timer <= 10 ? 'text-red-400' : ''}>⏰ {timer}s</span>
-          <span>Q {currentQuestion + 1}/{questions.length}</span>
+          <span className={timer <= 10 ? "text-red-400" : ""}>⏰ {timer}s</span>
+          <span>
+            Q {currentQuestion + 1}/{questions.length}
+          </span>
         </div>
       </div>
       <div className="max-w-2xl mx-auto bg-gray-800 p-6 rounded">
@@ -66,41 +69,39 @@ const QuizQuestions = () => {
             key={index}
             onClick={() => handleAnswerClick(option)}
             disabled={selectedAnswer !== ""}
-            className={`block w-full text-left px-4 py-2 mb-2 rounded ${selectedAnswer === option
-          ? option === currentQ.answer 
-            ? 'bg-green-600' 
-            : 'bg-red-600'
-          : 'bg-gray-700 hover:bg-gray-600'}`}
+            className={`block w-full text-left px-4 py-2 mb-2 rounded ${
+              selectedAnswer === option
+                ? option === currentQ.answer
+                  ? "bg-green-600"
+                  : "bg-red-600"
+                : "bg-gray-700 hover:bg-gray-600"
+            }`}
           >
             {option}
           </button>
         ))}
-            </div>
-            {selectedAnswer && selectedAnswer !== currentQ.answer && (
-  <div className="mt-4 p-3 bg-green-700 rounded">
-    <p className="text-green-100">✅ Correct Answer: {currentQ.answer}</p>
-  </div>
-)}
-            <button
-              onClick={handleNext}
-              className="mt-4 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
-              disabled={!selectedAnswer}
-            >
-              {isLastQuestion ? "Finish Quiz" : "Next Question"}
-            </button>
-        <div>
-         <button 
-              onClick={() => navigate('/')}
-              className="bg-green-500 hover:bg-green-600 px-6 py-2 rounded"
-            >
-              Back to Categories
-            </button>
-            </div>
-
-          </div>
-        
+      </div>
+      {selectedAnswer && selectedAnswer !== currentQ.answer && (
+        <div className="mt-4 p-3 bg-green-700 rounded">
+          <p className="text-green-100">✅ Correct Answer: {currentQ.answer}</p>
+        </div>
+      )}
+      <button
+        onClick={handleNext}
+        className="mt-4 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
+        disabled={!selectedAnswer}
+      >
+        {isLastQuestion ? "Finish Quiz" : "Next Question"}
+      </button>
+      <div>
+        <button
+          onClick={() => navigate("/")}
+          className="bg-green-500 hover:bg-green-600 px-6 py-2 rounded"
+        >
+          Back to Categories
+        </button>
+      </div>
+    </div>
   );
-}
+};
 export default QuizQuestions;
-    
-
